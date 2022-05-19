a83c610ffb5c3ef0878a2a3dca18f78ec6e342e6
David Negstad <David.Negstad@microsoft.com>
2022-05-19 15:18:28 -0700

    Update paths to clients

--- packages/vscode-container-runtimes/src/index.ts
+++ packages/vscode-container-runtimes/src/index.ts
@@ -2,8 +2,8 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
-export * from './clients/DockerClient';
-export * from './clients/PodmanClient';
+export * from './clients/DockerClient/DockerClient';
+export * from './clients/PodmanClient/PodmanClient';
 export * from './commandRunners/shell';
 export * from './commandRunners/wsl';
 export * from './utils/CancellationError';
