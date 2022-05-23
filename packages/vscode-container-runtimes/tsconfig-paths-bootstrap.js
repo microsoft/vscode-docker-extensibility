f4fc34dec7ccc37390202728bed9318a0b4ee060
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-05-23 13:15:19 -0400

    Update copyright statements to match vscode-docker (#42)

--- packages/vscode-container-runtimes/tsconfig-paths-bootstrap.js
+++ packages/vscode-container-runtimes/tsconfig-paths-bootstrap.js
@@ -1,5 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 // Helper to ensure vscode type placeholders can be loaded for tests
