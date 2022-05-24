f1e9503ed88e7b29efefd48384c92d1a484ee2ca
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-05-24 13:27:08 -0400

    More scaffolding of basic extension

--- packages/vscode-container-runtimes-extension/src/extension.ts
+++ packages/vscode-container-runtimes-extension/src/extension.ts
@@ -1,26 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
+import { PodmanClient, getDockerExtensionExport } from '@microsoft/vscode-container-runtimes';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-container-runtimes" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-container-runtimes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-container-runtimes!');
-	});
-
-	context.subscriptions.push(disposable);
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    try {
+        const dockerExtension = await getDockerExtensionExport();
+        context.subscriptions.push(dockerExtension.registerContainerRuntimeClient(new PodmanClient()));
+    } catch {
+        console.log('Bummer');
+    }
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {
+    // No-op
+}
