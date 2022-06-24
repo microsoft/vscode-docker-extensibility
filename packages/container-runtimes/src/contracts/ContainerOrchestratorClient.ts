2cb1fd16dd47649284b7a61305952cb26057d0e3
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-06-24 10:03:44 -0400

    Missed option

--- packages/container-runtimes/src/contracts/ContainerOrchestratorClient.ts
+++ packages/container-runtimes/src/contracts/ContainerOrchestratorClient.ts
@@ -81,6 +81,10 @@ export type DownCommandOptions = CommonOrchestratorCommandOptions & {
      * A timeout in seconds
      */
     timeoutSeconds?: number;
+    /**
+     * Additional custom options to pass
+     */
+    customOptions?: string;
 };
 
 type DownCommand = {
