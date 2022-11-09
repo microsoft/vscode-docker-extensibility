34ccb7a3d9168239aa7156262155406a1c52c20a
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-11-09 10:56:22 -0500

    Permit numeric since and until flags on events command

--- packages/container-runtimes/src/clients/DockerClientBase/DockerClientBase.ts
+++ packages/container-runtimes/src/clients/DockerClientBase/DockerClientBase.ts
@@ -257,8 +257,8 @@ export abstract class DockerClientBase extends ConfigurableClient implements ICo
     ): CommandLineArgs {
         return composeArgs(
             withArg('events'),
-            withNamedArg('--since', options.since),
-            withNamedArg('--until', options.until),
+            withNamedArg('--since', options.since?.toString(), { shouldQuote: !(typeof options.since === 'number') }),
+            withNamedArg('--until', options.until?.toString(), { shouldQuote: !(typeof options.until === 'number') }),
             withDockerLabelFilterArgs(options.labels),
             withNamedArg('--filter', options.types?.map((type) => `type=${type}`)),
             withNamedArg('--filter', options.events?.map((event) => `event=${event}`)),
