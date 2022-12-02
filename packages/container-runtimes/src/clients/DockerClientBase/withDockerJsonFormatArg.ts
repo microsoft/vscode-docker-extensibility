cb8f679f593f8648e3ebba7e4259d75fae0c59b0
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2022-12-02 16:09:10 -0500

    David's feedback

--- packages/container-runtimes/src/clients/DockerClientBase/withDockerJsonFormatArg.ts
+++ packages/container-runtimes/src/clients/DockerClientBase/withDockerJsonFormatArg.ts
@@ -3,6 +3,9 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { withNamedArg } from '../../utils/commandLineBuilder';
+import { withNamedArg, withVerbatimArg } from '../../utils/commandLineBuilder';
 
-export const withDockerJsonFormatArg = withNamedArg('--format', '"{{json .}}"');
+// Because Docker's wrapper script would split up `{{json .}}` into two arguments, we need to
+// pre-quote it to prevent that, for cases where we're executing without a shell.
+// Making it a verbatim argument also prevents it from being requoted later.
+export const withDockerJsonFormatArg = withNamedArg('--format', withVerbatimArg('"{{json .}}"')());
