d2331b8f3abb159646255b9cef6353681e14bb7c
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-08-29 12:42:26 -0400

    changed method names for dockerhub

--- packages/vscode-docker-registries/src/clients/DockerHub/DockerHubRegistryDataProvider.ts
+++ packages/vscode-docker-registries/src/clients/DockerHub/DockerHubRegistryDataProvider.ts
@@ -16,11 +16,11 @@ import * as vscode from 'vscode';
 
 export const DockerHubUrl = 'https://hub.docker.com/';
 
-export function isDockerHubRegistryItem(item: unknown): item is CommonRegistry {
+export function isDockerHubRegistry(item: unknown): item is CommonRegistry {
     return !!item && typeof item === 'object' && (item as CommonRegistry).additionalContextValues?.includes('dockerHubRegistry') === true;
 }
 
-export function isDockerHubRepositoryItem(item: unknown): item is CommonRepository {
+export function isDockerHubRepository(item: unknown): item is CommonRepository {
     return !!item && typeof item === 'object' && (item as CommonRepository).additionalContextValues?.includes('dockerHubRepository') === true;
 }
 
