8b8d128646c22ab305d6285f1a86fd4f3994ce23
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-09-07 16:43:07 -0400

    simplified showInputBox() implementation

--- packages/vscode-docker-registries/src/wizard/showInputBox.ts
+++ packages/vscode-docker-registries/src/wizard/showInputBox.ts
@@ -3,17 +3,34 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { InputBoxOptions, window } from 'vscode';
+import { CancellationError, InputBoxOptions, window } from 'vscode';
 import { RegistryWizardPromptStepOptions } from "./RegistryWizardPromptStep";
 
 export type InputBoxValidationResult = Awaited<ReturnType<Required<InputBoxOptions>['validateInput']>>;
 
-export async function showInputBox(options: RegistryWizardPromptStepOptions): Promise<string | undefined> {
-    return await window.showInputBox({
-        prompt: options.prompt,
-        placeHolder: options.placeholder,
-        ignoreFocusOut: true,
-        password: !!options.isSecretStep,
-        validateInput: options.validateInput,
+export async function showInputBox(options: RegistryWizardPromptStepOptions): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const inputBox = window.createInputBox();
+
+        inputBox.prompt = options.prompt;
+        inputBox.placeholder = options.placeholder;
+        inputBox.ignoreFocusOut = true;
+        inputBox.password = !!options.isSecretStep;
+
+        inputBox.onDidAccept(() => {
+            resolve(inputBox.value);
+            inputBox.dispose();
+        });
+        inputBox.onDidHide(() => {
+            reject(new CancellationError());
+            inputBox.dispose();
+        });
+        inputBox.onDidChangeValue(async text => {
+            if (options.validateInput) {
+                inputBox.validationMessage = await options.validateInput(text);
+            }
+        });
+
+        inputBox.show();
     });
 }
