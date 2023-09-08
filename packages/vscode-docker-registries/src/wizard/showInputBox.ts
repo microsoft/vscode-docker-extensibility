86bcc3ee7c049d583847c48c289575dc233d6b48
Alex Yang [MSFT] <59073590+alexyaang@users.noreply.github.com>
2023-09-08 10:10:17 -0400

    Simplified showInputBox() implementation (#188)
    * simplified showInputBox() implementation
    
    * removed unnecessary type

--- packages/vscode-docker-registries/src/wizard/showInputBox.ts
+++ packages/vscode-docker-registries/src/wizard/showInputBox.ts
@@ -3,17 +3,32 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { InputBoxOptions, window } from 'vscode';
+import { CancellationError, window } from 'vscode';
 import { RegistryWizardPromptStepOptions } from "./RegistryWizardPromptStep";
 
-export type InputBoxValidationResult = Awaited<ReturnType<Required<InputBoxOptions>['validateInput']>>;
+export async function showInputBox(options: RegistryWizardPromptStepOptions): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const inputBox = window.createInputBox();
 
-export async function showInputBox(options: RegistryWizardPromptStepOptions): Promise<string | undefined> {
-    return await window.showInputBox({
-        prompt: options.prompt,
-        placeHolder: options.placeholder,
-        ignoreFocusOut: true,
-        password: !!options.isSecretStep,
-        validateInput: options.validateInput,
+        inputBox.prompt = options.prompt;
+        inputBox.placeholder = options.placeholder;
+        inputBox.ignoreFocusOut = true;
+        inputBox.password = !!options.isSecretStep;
+
+        inputBox.onDidAccept(() => {
+            resolve(inputBox.value);
+            inputBox.dispose();
+        });
+        inputBox.onDidHide(() => {
+            reject(new CancellationError());
+            inputBox.dispose();
+        });
+        inputBox.onDidChangeValue(async text => {
+            if (options.validateInput) {
+                inputBox.validationMessage = await options.validateInput(text);
+            }
+        });
+
+        inputBox.show();
     });
 }
