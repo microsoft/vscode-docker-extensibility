aed0997d793e932330ab9739df28532bb527141a
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-03-04 13:44:01 -0500

    Commit current progress

--- packages/vscode-container-client/src/test/ContainersClientE2E.test.ts
+++ packages/vscode-container-client/src/test/ContainersClientE2E.test.ts
@@ -4,6 +4,32 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { expect } from 'chai';
+import { IContainersClient } from '../contracts/ContainerClient';
+import { DockerClient } from '../clients/DockerClient/DockerClient';
+import { PodmanClient } from '../clients/PodmanClient/PodmanClient';
+
+// Modify the below options to configure the tests
+const clientTypeToTest: ClientType = 'podman';
+
+// Supply to run the login/logout tests
+const dockerLogin = 'bwateratmsft';
+const dockerPAT = '';
+
+// No need to modify below this
+let client: IContainersClient;
+
+type ClientType = 'docker' | 'podman';
+
+switch (clientTypeToTest) {
+    case 'docker':
+        client = new DockerClient();
+        break;
+    case 'podman':
+        client = new PodmanClient();
+        break;
+    default:
+        throw new Error(`Unsupported client type: ${clientTypeToTest}`);
+}
 
 describe('(integration) ContainersClientE2E', function () {
     this.timeout(10000); // Set a longer timeout for integration tests
