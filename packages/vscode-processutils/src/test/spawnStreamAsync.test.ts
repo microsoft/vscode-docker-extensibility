6e94fcaa303ac4b4177dc58706c2e60d97925444
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-07-15 10:59:00 -0400

    Finish out the tests

--- packages/vscode-processutils/src/test/spawnStreamAsync.test.ts
+++ packages/vscode-processutils/src/test/spawnStreamAsync.test.ts
@@ -3,30 +3,69 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { expect } from 'chai';
 import { AccumulatorStream } from '../utils/AccumulatorStream';
-import { NoShell } from '../utils/Shell';
+import { composeArgs, withArg, withNamedArg } from '../utils/commandLineBuilder';
+import { NoShell, Shell } from '../utils/Shell';
 import { spawnStreamAsync } from '../utils/spawnStreamAsync';
 
+// NOTE: This integration test requires Docker to be installed and running
+// It will automatically pull the `alpine:latest` image if not already present
+
+// The command lists Docker images using some relatively complex arguments (at
+// least in terms of quoting and escaping)
+const command = 'docker';
+const args = composeArgs(
+    withArg('image', 'ls'),
+    withNamedArg('--filter', 'dangling=false', { shouldQuote: true }),
+    withArg('--no-trunc'),
+    withNamedArg('--format', '{{json .}}', { shouldQuote: true }),
+)();
+
 describe('(integration) spawnStreamAsync', () => {
-    it('Should work on Windows please', async () => {
-        const command = "docker";
-        const args = ['container', 'ls', '--all'];
+    before(async () => {
+        const args = composeArgs(
+            withArg('pull', 'alpine:latest'),
+        )();
 
-        const errAccumulator = new AccumulatorStream();
+        await spawnStreamAsync(command, args, {
+            shellProvider: new NoShell(),
+        });
+    });
+
+    it('Should be able to run complex commands without a shell', async () => {
         const outAccumulator = new AccumulatorStream();
+        const errAccumulator = new AccumulatorStream();
 
-        try {
-            await spawnStreamAsync(command, args, {
-                stdOutPipe: outAccumulator,
-                stdErrPipe: errAccumulator,
-                //shellProvider: Shell.getShellOrDefault(),
-                shellProvider: new NoShell(),
-            });
-        } catch (error) {
-            console.error('Error executing command:', error);
-        }
+        await spawnStreamAsync(command, args, {
+            stdOutPipe: outAccumulator,
+            stdErrPipe: errAccumulator,
+            shellProvider: new NoShell(),
+        });
 
-        console.log('stdout:', await outAccumulator.getString());
-        console.log('stderr:', await errAccumulator.getString());
+        const output = await outAccumulator.getString();
+        expect(output).to.not.be.empty;
+        expect(output).to.include('"Repository":"alpine"');
+        expect(output).to.include('"Tag":"latest"');
+
+        expect(await errAccumulator.getString()).to.be.empty;
+    });
+
+    it('Should be able to run complex commands with the default shell', async () => {
+        const outAccumulator = new AccumulatorStream();
+        const errAccumulator = new AccumulatorStream();
+
+        await spawnStreamAsync(command, args, {
+            stdOutPipe: outAccumulator,
+            stdErrPipe: errAccumulator,
+            shellProvider: Shell.getShellOrDefault(),
+        });
+
+        const output = await outAccumulator.getString();
+        expect(output).to.not.be.empty;
+        expect(output).to.include('"Repository":"alpine"');
+        expect(output).to.include('"Tag":"latest"');
+
+        expect(await errAccumulator.getString()).to.be.empty;
     });
 });
