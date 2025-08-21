ad0510627daeca5df99edc20d00326f281c76af2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-08-21 17:13:17 -0400

    Update `@types/vscode` to 1.96 and use new `vscode.IconPath` type (#287)

--- packages/vscode-docker-registries/src/contracts/RegistryDataProvider.ts
+++ packages/vscode-docker-registries/src/contracts/RegistryDataProvider.ts
@@ -11,7 +11,7 @@ export interface RegistryDataProvider<T extends RegistryItem> extends vscode.Tre
     readonly id: string;
     readonly label: string;
     readonly description?: string;
-    readonly iconPath?: string | vscode.Uri | { light: string | vscode.Uri; dark: string | vscode.Uri } | vscode.ThemeIcon;
+    readonly iconPath?: vscode.IconPath;
 
     getLoginInformation?(item: T): Promise<LoginInformation> | LoginInformation;
 
