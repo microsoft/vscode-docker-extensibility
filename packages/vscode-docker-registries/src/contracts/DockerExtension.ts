f13ffa8580559564c63f4394fc9fc68112d3d8dd
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-04-28 09:34:39 -0400

    Change some imports to type

--- packages/vscode-docker-registries/src/contracts/DockerExtension.ts
+++ packages/vscode-docker-registries/src/contracts/DockerExtension.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from 'vscode';
+import type * as vscode from 'vscode';
 import { RegistryDataProvider } from './RegistryDataProvider';
 import { RegistryItem } from './RegistryItem';
 
